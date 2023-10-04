ab4679ed1a4b0afda833d8a580e8911a8a3c7e73
Anthony4m <amoahanthony41@gmail.com>
2023-10-04 11:28:43 +0100

    Added socket handle to handle the soclet handshake

---
+++ Tnet/SocketHandle.h
@@ -0,0 +1,13 @@
+//
+// Created by pepos on 10/3/2023.
+//
+
+#ifndef WINSTESTS_SOCKETHANDLE_H
+#define WINSTESTS_SOCKETHANDLE_H
+#define WIN32_LEAN_AND_MEAN
+#include <Winsock2.h>
+
+namespace Tnet{
+    typedef SOCKET SocketHandle;
+}
+#endif //WINSTESTS_SOCKETHANDLE_H
