8aac52d32b3c2494fef6ee16d13ccaabecad90b3
Anthony4m <amoahanthony41@gmail.com>
2023-10-04 11:06:50 +0100

    Added tresults to check the results of the connection

---
+++ Tnet/tresults.h
@@ -0,0 +1,13 @@
+//
+// Created by pepos on 10/3/2023.
+//
+#ifndef WINSTESTS_TRESULTS_H
+#define WINSTESTS_TRESULTS_H
+
+namespace Tnet{
+    enum class TResults{
+        T_Success,
+        T_NotYetImplemented
+    };
+}
+#endif //WINSTESTS_TRESULTS_H
