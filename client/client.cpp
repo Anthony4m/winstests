3c1a8963fdfccf6a39e64f9073366f58148f2fa8
Anthony4m <amoahanthony41@gmail.com>
2023-10-04 11:32:12 +0100

    Updated socket connection on server and client for creating and closing socket connection with the library

--- client/client.cpp
+++ client/client.cpp
@@ -4,6 +4,13 @@ using namespace Tnet;
 int main() {
     if(Network::Initialize()) {
         std::cout << "Initialized" << std::endl;
+        Socket socket;
+        if (socket.Create() == TResults::T_Success){
+            std::cout<<"Socket Successfully created" <<std::endl;
+            socket.Close();
+        } else{
+            std::cout<<"Socket failed to  create" <<std::endl;
+        }
     }
     Network::Shutdown();
 
--- server/server.cpp
+++ server/server.cpp
@@ -2,9 +2,27 @@
 #include "../Tnet/Tnet.h"
 using namespace Tnet;
 int main() {
-//    setsockopt()
     if(Network::Initialize()) {
         std::cout << "Initialized" << std::endl;
+        IPEndpoint test("www.google.com", 8080);
+        if (test.GetIPversion() == IPVersion::IPv4){
+            std::cout<<"Hostname "<< test.GetHostname() <<std::endl;
+            std::cout<<"IP Address "<< test.GetIPAddress() <<std::endl;
+            std::cout<<"Port "<< test.GetPort() <<std::endl;
+            std::cout<<"IPv4 " <<std::endl;
+            for(auto & digit : test.GetIPBytes()){
+                std::cout<<(int)digit <<std::endl;
+            }
+        } else{
+            std::cout<<"This is not an IPv4" <<std::endl;
+        }
+        Socket socket;
+        if (socket.Create() == TResults::T_Success){
+            std::cout<<"Socket Successfully created" <<std::endl;
+            socket.Close();
+        } else{
+            std::cout<<"Socket failed to  create" <<std::endl;
+        }
     }
     Network::Shutdown();
 
