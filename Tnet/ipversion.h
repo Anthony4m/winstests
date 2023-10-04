0a5503cd721e15f3091b3d61bc9184a9d9f39575
Anthony4m <amoahanthony41@gmail.com>
2023-10-04 11:33:38 +0100

    Added IPVersion handling

---
+++ Tnet/ipversion.h
@@ -0,0 +1,11 @@
+#ifndef WINSTESTS_IPVERSION_H
+#define WINSTESTS_IPVERSION_H
+
+namespace Tnet{
+    enum class IPVersion{
+        Unknown,
+        IPv4,
+        IPv6
+    };
+}
+#endif //WINSTESTS_IPVERSION_H
