9b5b5e73e75ae316a33b58526f6eea51e7fa352b
Anthony4m <amoahanthony41@gmail.com>
2023-10-05 18:19:36 +0100

    Added connection and packet sending abilities to the client server

--- client/client.cpp
+++ client/client.cpp
@@ -7,12 +7,28 @@ int main() {
         Socket socket;
         if (socket.Create() == TResults::T_Success){
             std::cout<<"Socket Successfully created" <<std::endl;
+            // connect to server on 1995
+            if (socket.Connect(IPEndpoint("localhost",1996)) == TResults::T_Success){
+                std::cout<<"Socket Successfully Connected" <<std::endl;
+                char buffer[1024];
+                strcpy(buffer,"Hello World from client");
+                auto result = TResults::T_Success;
+                while (result == TResults::T_Success){
+                    result = socket.SendAll(buffer,sizeof(buffer));
+                    std::cout<<"Sending Data.....: "<<buffer<<std::endl;
+                    Sleep(500);
+                };
+                std::cout << "Socket failed to send" << std::endl;
+            } else{
+                std::cout<<"Socket failed to connect" <<std::endl;
+            }
             socket.Close();
         } else{
             std::cout<<"Socket failed to  create" <<std::endl;
         }
     }
     Network::Shutdown();
+    system("pause");
 
 
     return 0;
