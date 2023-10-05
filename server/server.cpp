7c2bd302de04d69f90e31dbcfa6abe1aeb8b5dd2
Anthony4m <amoahanthony41@gmail.com>
2023-10-05 18:11:06 +0100

    made socket bind to port 1996 and accept connections comming to that port

--- server/server.cpp
+++ server/server.cpp
@@ -4,28 +4,40 @@ using namespace Tnet;
 int main() {
     if(Network::Initialize()) {
         std::cout << "Initialized" << std::endl;
-        IPEndpoint test("www.google.com", 8080);
-        if (test.GetIPversion() == IPVersion::IPv4){
-            std::cout<<"Hostname "<< test.GetHostname() <<std::endl;
-            std::cout<<"IP Address "<< test.GetIPAddress() <<std::endl;
-            std::cout<<"Port "<< test.GetPort() <<std::endl;
-            std::cout<<"IPv4 " <<std::endl;
-            for(auto & digit : test.GetIPBytes()){
-                std::cout<<(int)digit <<std::endl;
-            }
-        } else{
-            std::cout<<"This is not an IPv4" <<std::endl;
-        }
+        //server to bind port 1996
+
         Socket socket;
         if (socket.Create() == TResults::T_Success){
             std::cout<<"Socket Successfully created" <<std::endl;
+            // server to listen for connections on 1995
+            if (socket.Listen(IPEndpoint("0.0.0.0",1996)) == TResults::T_Success){
+                std::cout<<"Socket Successfully Listening to 1996" <<std::endl;
+                Socket clientConnection;
+                if (socket.Accept(clientConnection) == TResults::T_Success){
+                    std::cout<<"Socket Successfully Accepted" <<std::endl;
+                    char buffer[1024];
+                    int bytesReceived = 0;
+                    auto result = TResults::T_Success;
+                    while (result == TResults::T_Success){
+                        result = clientConnection.RecvAll(buffer,sizeof(buffer));
+                        std::cout<<"Receiving Data.....: "<<buffer<<std::endl;
+                        if (result != TResults::T_Success){
+                            std::cout<<"Socket failed to receive" <<std::endl;
+                            break;
+                            std::cout<<buffer<<std::endl;
+                        }
+                    }
+                }
+            } else{
+                std::cout<<"Socket failed to listen" <<std::endl;
+            }
             socket.Close();
         } else{
             std::cout<<"Socket failed to  create" <<std::endl;
         }
     }
     Network::Shutdown();
-
+    system("pause");
 
     return 0;
 }
