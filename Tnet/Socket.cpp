4dde237d020ecc3e31b685824fe5a3bf15f430ba
Anthony4m <amoahanthony41@gmail.com>
2023-10-05 18:10:10 +0100

    Added implementations of new socket fuctions for connection and sending packets

--- Tnet/Socket.cpp
+++ Tnet/Socket.cpp
@@ -6,6 +6,8 @@
 #include "ipversion.h"
 #include "Socket.h"
 #include <cassert>
+#include <iostream>
+
 namespace Tnet{
         Socket::Socket(IPVersion ipversion , SocketHandle handle )
         :ipversion(ipversion),handle(handle) {
@@ -68,5 +70,102 @@ namespace Tnet{
         return TResults::T_Success;
 
     }
+
+    TResults Socket::Bind(IPEndpoint endPoint) {
+            sockaddr_in addr = endPoint.GetSockAddr();
+            int result = bind(handle, (sockaddr *)(&addr), sizeof(sockaddr_in));
+        if (result != 0){
+            int error = WSAGetLastError();
+            return TResults::T_NotYetImplemented;
+        }
+        return TResults::T_Success;
+    }
+
+    TResults Socket::Listen(IPEndpoint endPoint, int backlog) {
+            if (Bind(endPoint) != TResults::T_Success){
+                return TResults::T_NotYetImplemented;
+            }
+            int result = listen(handle, backlog);
+            if (result != 0){
+                int error = WSAGetLastError();
+                return TResults::T_NotYetImplemented;
+            }
+            return TResults::T_Success;
+    }
+
+    TResults Socket::Accept(Socket &socket) {
+            sockaddr_in addr = {};
+            int len = sizeof(sockaddr_in);
+            SocketHandle acceptConnectionHandle = accept(handle, (sockaddr *)&addr, &len);
+            if (acceptConnectionHandle == INVALID_SOCKET){
+                int error = WSAGetLastError();
+                return TResults::T_NotYetImplemented;
+            }
+            IPEndpoint connectionEndPoint((sockaddr*)&addr);
+            std::cout << "Connection from " << connectionEndPoint.GetIPAddress() << ":" << connectionEndPoint.GetPort() << std::endl;
+            connectionEndPoint.Print();
+            socket = Socket(IPVersion::IPv4, acceptConnectionHandle);
+        return TResults::T_Success;
+    }
+
+    TResults Socket::Connect(IPEndpoint endPoint) {
+            sockaddr_in addr = endPoint.GetSockAddr();
+            int result = connect(handle, (sockaddr *)(&addr), sizeof(sockaddr_in));
+        if (result != 0){
+            int error = WSAGetLastError();
+            return TResults::T_NotYetImplemented;
+        }
+        return TResults::T_Success;
+    }
+
+    TResults Socket::Send(void *data, size_t size, int &bytesSent) {
+            bytesSent = send(handle, (const char *)data, size, 0);
+        if (bytesSent == SOCKET_ERROR){
+            int error = WSAGetLastError();
+            return TResults::T_NotYetImplemented;
+        }
+        return TResults::T_Success;
+    }
+
+    TResults Socket::Recv(void *data, size_t size, int &bytesReceived) {
+            bytesReceived = recv(handle, (char *)data, size, 0);
+            if (bytesReceived == 0){
+                return TResults::T_NotYetImplemented;
+            }
+        if (bytesReceived == SOCKET_ERROR){
+            int error = WSAGetLastError();
+            return TResults::T_NotYetImplemented;
+        }
+        return TResults::T_Success;
+    }
+
+    TResults Socket::SendAll(void *data, size_t size) {
+        int totalBytesSent = 0;
+        while (totalBytesSent < size){
+            int bytesRemaining = size - totalBytesSent;
+            char *dataRemaining = (char *)data + totalBytesSent;
+            int bytesSent = send(handle, (const char *)data + totalBytesSent, size - totalBytesSent, 0);
+            TResults result = Send(dataRemaining, bytesRemaining, bytesSent);
+            if (result != TResults::T_Success){
+                return TResults::T_NotYetImplemented;
+            }
+            totalBytesSent += bytesSent;
+        }
+        return TResults::T_Success;
+    };
+    TResults Socket::RecvAll(void *data, size_t size) {
+        int totalBytesReceived = 0;
+        while (totalBytesReceived < size){
+            int bytesRemaining = size - totalBytesReceived;
+            char *dataRemaining = (char *)data + totalBytesReceived;
+            int bytesSent = recv(handle, ( char *)data + totalBytesReceived, size - totalBytesReceived, 0);
+            TResults result = Send(dataRemaining, bytesRemaining, bytesSent);
+            if (result != TResults::T_Success){
+                return TResults::T_NotYetImplemented;
+            }
+            totalBytesReceived += bytesSent;
+        }
+        return TResults::T_Success;
+    };
 
 }
