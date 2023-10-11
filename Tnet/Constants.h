05d637ebb1805f5b14939abf21d3dce6648b21c3
Anthony4m <amoahanthony41@gmail.com>
2023-10-11 22:01:31 +0100

    max buffer constants

---
+++ Tnet/Constants.h
@@ -0,0 +1,12 @@
+//
+// Created by pepos on 10/5/2023.
+//
+
+#ifndef WINSTESTS_CONSTANTS_H
+#define WINSTESTS_CONSTANTS_H
+namespace Tnet {
+    const int MAX_BUFFER_SIZE = 8192;
+    const int MAX_PACKET_SIZE = 8192;
+
+}
+#endif //WINSTESTS_CONSTANTS_H
