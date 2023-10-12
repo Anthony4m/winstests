85bd73aabbedffef47f816223bac9ed80eb6ecc5
Anthony4m <amoahanthony41@gmail.com>
2023-10-12 18:22:06 +0100

    Added implementation for getting socket address and printing the details of the connecting client server

--- Tnet/IPEndPoint.h
+++ Tnet/IPEndPoint.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <cstdint>
+#include <WS2tcpip.h>
 
 #ifndef WINSTESTS_IPENDPOINT_H
 #define WINSTESTS_IPENDPOINT_H
@@ -12,11 +13,14 @@ namespace Tnet {
     class IPEndpoint {
     public:
         IPEndpoint(const char * ipaddress, unsigned short port);
+        IPEndpoint(sockaddr * host_addr);
         IPVersion GetIPversion();
         std::string GetHostname();
         std::string GetIPAddress();
         std::vector<uint8_t> GetIPBytes();
         unsigned short GetPort();
+        sockaddr_in GetSockAddr();
+        void Print();
     private:
          IPVersion ipversion = IPVersion::Unknown;
          std::string hostname;
